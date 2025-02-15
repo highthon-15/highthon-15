72e0b0840093d1199d1d18c54c05aa3f5a05a463
유지민 <a01040987587@gmail.com>
2025-02-15 23:14:00 +0900

    refactor: 로그인 여부 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,10 +26,6 @@ export default function Page() {
     }
   }, [session, mutate]);
 
-  if (!session) {
-    return <div>로그인 후 사용 가능합니다.</div>;
-  }
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
