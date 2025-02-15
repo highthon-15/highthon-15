acd3f85fc6d2d94ee3ced5791e8d0783fe117a9d
gunggme <ekfrifajfl2019@gmail.com>
2025-02-15 22:43:46 +0900

    feat : 추천퀴즈 섹션 완료

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import QuizCard from "@/components/ui/quizcard";
 import PopularQuizCard from "@/components/ui/popularquizcard";
 
 export default function MainPage() {
@@ -12,6 +13,23 @@ export default function MainPage() {
                     <PopularQuizCard ranknumber={1} />
                     <PopularQuizCard ranknumber={2} />
                     <PopularQuizCard ranknumber={3} />
+                </div>
+            </div>
+
+            {/*추천 퀴즈 섹션*/}
+            <div className="flex w-full h-[50rem] flex-col gap-8 mt-12 items-center">
+                <div className="flex flex-row w-3/6 justify-between">
+                    <h2 className="text-4xl font-extrabold">추천 퀴즈</h2>
+                </div>
+                <div className="flex w-full h-full gap-4 items-center justify-center">
+                    <div className="grid grid-cols-3 w-3/6 h-full gap-4 items-center justify-center">
+                        <QuizCard />
+                        <QuizCard />
+                        <QuizCard />
+                        <QuizCard />
+                        <QuizCard />
+                        <QuizCard />
+                    </div>
                 </div>
             </div>
         </div>
