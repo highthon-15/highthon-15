72a5a659aac5805eaec2d8fe3f7aa34b51eb0f0b
gunggme <ekfrifajfl2019@gmail.com>
2025-02-15 21:35:35 +0900

    feat : 인기 퀴즈 카드 완성

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,11 +2,11 @@ import PopularQuizCard from "@/components/ui/popularquizcard";
 
 export default function MainPage() {
     return (
-        <div className="flex flex-col mx-auto px-4 py-8 items-center">
+        <div className="flex w-full h-full flex-col mx-auto py-8 items-center">
             {/*인기 퀴즈 섹션*/}
-            <div className="flex flex-col gap-8 items-center">
+            <div className="flex w-full h-full flex-col gap-8 items-center">
                 <h2 className="text-3xl font-bold mb-4">인기 퀴즈</h2>
-                <div className="flex flex-col md:grid-cols-3 gap-2 items-center">
+                <div className="flex flex-col w-full md:grid-cols-3 gap-4 items-center">
                     <PopularQuizCardWithRankNumber ranknumber={1} />
                     <PopularQuizCardWithRankNumber ranknumber={2} />
                     <PopularQuizCardWithRankNumber ranknumber={3} />
@@ -18,8 +18,10 @@ export default function MainPage() {
 
 const PopularQuizCardWithRankNumber = ({ ranknumber }: { ranknumber: number }) => {
     return (
-        <div className="h-full w-full bg-white flex flex-row">
-            <span className="text-6xl font-bold">{ranknumber}</span>
+        <div className="h-60 w-3/5 bg-white flex flex-row items-center">
+            <div className="flex flex-row items-center w-1/12 justify-center">
+                <span className="text-6xl font-bold">{ranknumber}</span>
+            </div>
             <PopularQuizCard />
         </div>
     )
--- src/components/ui/popularquizcard.tsx
+++ src/components/ui/popularquizcard.tsx
@@ -1,7 +1,30 @@
+import { StarIcon } from "lucide-react";
+
+// todo 데이터 타입 만들어서 참조필요
 export default function PopularQuizCard() {
     return (
-        <div className="flex w-full p-6 bg-white">
-            <h2 className="text-xl font-bold mb-4">인기 퀴즈 (나중에 데이터 클래스 만들어서 사용 예정)</h2>
+        <div className="flex w-full h-full shadow-xl rounded-xl">
+            {/* 대표 사진 */}
+            <div className="flex flex-col w-1/4 h-full bg-red-500">
+            </div>
+            {/* 퀴즈 정보 */}
+            <div className="flex flex-col w-3/4 h-full">
+                {/* 퀴즈 제목 */}
+                <div className="flex flex-col w-full h-1/4 pt-4 px-3 justify-center">
+                    <h2 className="text-3xl font-bold mb-4">인기 퀴즈 (나중에 데이터 클래스 만들어서 사용 예정)</h2>
+                </div>
+                {/* 퀴즈 별점 */}
+                <div className="flex flex-row w-full h-3/4 space-x-2 px-3 py-2">
+                    <StarIcon className="w-10 h-10" />
+                    <h3 className="text-2xl font-bold">4.5</h3>
+                </div>
+                {/* 퀴즈 태그 */}
+                <div className="flex flex-row w-full h-3/6 items-center">
+                    <div className="flex h-1/2 flex-row border-2 border-black items-center justify-center px-3 mx-3 my-2 rounded-md">
+                        <p className="text-sm font-bold">애니메이션</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
