96e7df0d1da3bdf5da82133fe818dd7b80239bb3
gunggme <ekfrifajfl2019@gmail.com>
2025-02-16 03:01:04 +0900

    fix : useclient 누락 해결

--- src/app/quiz/question/[quizId]/page.tsx
+++ src/app/quiz/question/[quizId]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, {useState} from 'react';
 import {SquareChevronRight} from 'lucide-react';
 import {useFetchQuestionsQuery} from '@/hooks/useFetchQuestionsQuery';
--- src/components/profile/QuizHistoryWidget.tsx
+++ src/components/profile/QuizHistoryWidget.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { QuizHistory } from '@/types/user';
 import Link from 'next/link';
 import Image from 'next/image';
