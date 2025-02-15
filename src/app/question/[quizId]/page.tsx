331b70678ee779ecd591d23fc5ca40aa10a961e1
유지민 <a01040987587@gmail.com>
2025-02-16 06:13:05 +0900

    refactor: url 수정

--- src/app/question/[quizId]/page.tsx
+++ src/app/question/[quizId]/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
       {
         onSettled: () => {
           setIsSubmitting(false);
-          router.push(`/result/${quizId}`);
+          router.push(`/quiz/result/${quizId}`);
         },
       },
     );
