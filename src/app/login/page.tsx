21e07d8ee4d1aba2f3712e597c475fe341d5f3c5
gunggme <ekfrifajfl2019@gmail.com>
2025-02-16 10:55:46 +0900

    feat : login image add

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Page() {
   const handleKakaoLogin = () => {
@@ -9,7 +10,8 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+    <div className="flex flex-col items-center justify-start h-screen space-y-4">
+      <Image src="/2953982.jpg" alt="logo" width={700} height={700} />
       <Button
         onClick={handleKakaoLogin}
         className="flex items-center justify-center bg-yellow-400 text-black font-bold py-2 px-6 rounded-lg w-1/4">
